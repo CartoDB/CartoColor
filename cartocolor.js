6d915b7db2c01803aaab621eb4f8cb7f4e5cd934
andrewxhill <andrewxhill@gmail.com>
2016-04-14 12:14:10 -0400

    first commit

---
+++ cartocolor.js
@@ -0,0 +1,15 @@
+!function() {
+
+var cartocolor = {
+
+};
+
+if (typeof define === "function" && define.amd) {
+    define(colorbrewer);
+} else if (typeof module === "object" && module.exports) {
+    module.exports = colorbrewer;
+} else {
+    this.colorbrewer = colorbrewer;
+}
+
+}();
